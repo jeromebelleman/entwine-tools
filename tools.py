4687632c3b8ea43aff9850d1aa769c3d691df3b4
Jerome Belleman <jerome.belleman@gmail.com>
2014-08-02 17:57:23 +0200

    Only make thumbnail if original changed

--- tools.py
+++ tools.py
@@ -15,7 +15,9 @@ def photos(params):
         pass
     for entry in os.listdir('.'):
         if entry.lower().endswith('.jpg'):
-            subprocess.call(['gm', 'convert', '-resize', 'x64', entry,
-                             'thumbnails/' + entry])
+            path = 'thumbnails/' + entry
+            if os.stat(entry).st_mtime > os.stat(path).st_mtime:
+                subprocess.call(['gm', 'convert', '-resize', 'x64',
+                                 entry, path])
             print '[![%s](thumbnails/%s)](%s)' % ((entry,) * 3)
 
