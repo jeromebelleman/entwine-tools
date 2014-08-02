e6060e29aeff9a7f40367dd8386fd94d123c541d
Jerome Belleman <jerome.belleman@gmail.com>
2014-08-02 16:50:54 +0200

    Create thumbnails

--- tools.py
+++ tools.py
@@ -1,15 +1,21 @@
 import os
+import subprocess
 
 def photos(params):
     params['styles'] = '''
        img {
-           height: 64px;
            border: thin solid;
            padding: 2px;
        }
     '''
 
+    try:
+        os.mkdir('thumbnails')
+    except OSError:
+        pass
     for entry in os.listdir('.'):
         if entry.endswith('.jpg'):
-            print '[![%s](%s)](%s)' % ((entry,) * 3)
+            subprocess.call(['gm', 'convert', '-resize', 'x64', entry,
+                             'thumbnails/' + entry])
+            print '[![%s](thumbnails/%s)](%s)' % ((entry,) * 3)
 
