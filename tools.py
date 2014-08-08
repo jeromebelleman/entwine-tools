00132a7c5c3d89b554d0ca0fc88016a67243487d
Jerome Belleman <jerome.belleman@gmail.com>
2014-08-08 15:29:58 +0200

    Optionally display picture details

--- tools.py
+++ tools.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-def photos(params, start=0, stop=None, size=64):
+def photos(params, start=0, stop=None, size=64, details=False):
     params['styles'] = '''
        img {
            border: thin solid;
@@ -16,7 +16,7 @@ def photos(params, start=0, stop=None, size=64):
         pass
 
     # Make entries
-    def mkentry(entry):
+    def mkentry(entry, details):
         path = 'thumbnails/' + entry
         try:
             if os.stat(entry).st_mtime > os.stat(path).st_mtime:
@@ -24,11 +24,26 @@ def photos(params, start=0, stop=None, size=64):
         except OSError:
             subprocess.call(['gm', 'convert', '-resize', 'x' + str(size),
                              entry, path])
-        print '[![%s](thumbnails/%s)](%s)' % ((entry,) * 3)
+        if details:
+            filesize = os.stat(entry).st_size / 1024
+            proc = subprocess.Popen(['file', entry], stdout=subprocess.PIPE)
+            filetype, _ = proc.communicate()
+            row = '<tr><td>[![%s](thumbnails/%s)](%s)</td>'
+            row += '<td>%s, %s kB</td></tr>'
+            print row % ((entry,) * 3 + (filetype.strip(), filesize))
+        else:
+            print '[![%s](thumbnails/%s)](%s)' % ((entry,) * 3)
 
     entries = [entry for entry in os.listdir('.')
                if entry.lower().endswith('.jpg') or
                entry.lower().endswith('.png')]
+
+    if details:
+        print '<table>'
+
     for entry in entries[start:stop]:
-        mkentry(entry)
+        mkentry(entry, details)
+
+    if details:
+        print '</table>'
 
